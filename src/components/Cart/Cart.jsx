6b7aa64ba61420ad52baffaa50e9efb844715e67
sebastiangallego0317 <samuelmontes1105@gmail.com>
2023-12-06 17:16:03 -0500

    fixed color and response things

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,30 +1,19 @@
 import React, { useContext, useRef } from "react";
 import { CartContext } from "../CartContext";
 import "./Cart.css";
+import { CartItem } from "./CartItem";
 
-const Cart = () => {
-  const {
-    cartItems,
-    removeFromCart,
-    clearCart,
-    incrementQuantity,
-    decrementQuantity,
-  } = useContext(CartContext);
+const MappedItems = () => {
+  const { cartItems } = useContext(CartContext);
+  return cartItems.map((item) => <CartItem item={item} />);
+};
+
+export default function cart() {
+  const { cartItems, clearCart } = useContext(CartContext);
   const modalRef = useRef(null);
-  const handleRemoveFromCart = (item) => {
-    removeFromCart(item);
-  };
 
   const handleClearCart = () => {
     clearCart();
-  };
-
-  const handleIncrementQuantity = (item) => {
-    incrementQuantity(item);
-  };
-
-  const handleDecrementQuantity = (item) => {
-    decrementQuantity(item);
   };
 
   const handleConfirmPurchase = () => {
@@ -44,41 +33,24 @@ const Cart = () => {
         <p className="cart-empty">No hay items.</p>
       ) : (
         <ul className="cart-items">
-          {cartItems.map((item) => (
-            <li key={item.id} className="cart-item">
-              <strong>{item.title}</strong> - $
-              {(item.price * item.quantity).toFixed(2)}
-              <button
-                onClick={() => handleRemoveFromCart(item.id)}
-                className="cart-remove-button"
-              >
-                Eliminar
-              </button>
-              <button
-                onClick={() => handleIncrementQuantity(item)}
-                className="cart-increment-button"
-              >
-                +
-              </button>
-              <button
-                onClick={() => handleDecrementQuantity(item)}
-                className="cart-decrement-button"
-              >
-                -
-              </button>
-              {item.quantity}
-            </li>
-          ))}
+          <MappedItems />
         </ul>
       )}
-      <button onClick={handleClearCart} >
-        Limpiar carrito
-      </button>
-      <button disabled={cartItems.length === 0} className="cart-clear-button" onClick={handleConfirmPurchase}>
+      <button onClick={handleClearCart}>Limpiar carrito</button>
+      <button
+        disabled={cartItems.length === 0}
+        className="cart-clear-button"
+        onClick={handleConfirmPurchase}
+      >
         Comprar
       </button>
       <dialog className="modal" ref={modalRef}>
         <div className="modal-style">
+          <div className="modal-items">
+            <ul className="cart-items">
+              <MappedItems />
+            </ul>
+          </div>
           <label> Confirmar compra</label>
           <button onClick={handleCancelPurchase}> Cancelar </button>
           <button onClick={handleBuy}>Comprar </button>
@@ -86,6 +58,4 @@ const Cart = () => {
       </dialog>
     </div>
   );
-};
-
-export default Cart;
+}
